5964db3b7a227657a281e1a6eae15ffbcb0d3472
haront7002-sketch <haron.t.7002@gmail.com>
2025-10-29 11:49:07 +0200

    intial commit

---
+++ exercise1-week3-Taaraa_Hron.js
@@ -0,0 +1,17 @@
+const firstName = "Taaraa";
+const lastName = "Haron";
+let age = 18;
+
+const fullName = firstName + " " + lastName;
+console.log("Hello my name is", fullName);
+
+console.log("I am", age);
+
+age += 1;
+console.log("I am turning", age);
+
+age += 7;
+console.log("I'll get married when i am", age);
+
+const marriedLastname = "Talmarkes";
+console.log("My last name will be", marriedLastname);
